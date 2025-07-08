767cf51a806018e8af9f73d795f243b6fa80c037
Maximilian Wei <81603715+inno-apfel@users.noreply.github.com>
2025-07-07 19:54:29 -0700

    build visible auth pages

---
+++ src/components/signup-form.tsx
@@ -0,0 +1,61 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign up to ParamLogger</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Continue
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Continue with Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign in
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
--- src/pages/signup-page.jsx
+++ src/pages/signup-page.jsx
@@ -1,7 +1,11 @@
+import { SignupForm } from "@/components/signup-form"
+
 const SignupPage = () => {
   return (
-    <div>
-      <h1>SIGN UP PAGE</h1>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm />
+      </div>
     </div>
   );
 };
