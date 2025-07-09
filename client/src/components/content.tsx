4bca2e66fb114cbe2b372d9884578c01bec77542
Maximilian Wei <81603715+inno-apfel@users.noreply.github.com>
2025-07-08 19:06:07 -0700

    test clientserver connection

--- client/src/components/content.tsx
+++ client/src/components/content.tsx
@@ -7,9 +7,26 @@ import {
 
 import { ParamChart } from "@/components/param-chart"
 
+import axios from 'axios'
+
+import { useState, useEffect, type JSX } from 'react'
+
 function Content() {
 
   const nums =  Array.from({ length: 6 }, (_, x) => x);
+
+  const [array, setArray] = useState([]);
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    // console.log(response.data.params);
+    setArray(response.data.params);
+    console.log(array);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
   return (
     <div className="grid grid-cols-2 grow-5">
@@ -22,7 +39,9 @@ function Content() {
           <hr className="-mx-6 mt-4  border-gray-100" />
         </CardHeader>
         <CardContent>
-            Lorem Ipsum
+            {array.map((fruit: string): JSX.Element => (
+              <p>{fruit}</p>
+            )) }
         </CardContent>
       </Card>
 
