b4b8e59049e5a77624874ef20cf1f983d7893063
Maximilian Wei <81603715+inno-apfel@users.noreply.github.com>
2025-07-18 19:34:26 -0700

    Update to auto-navigate to /my-tanks if user is already logged-in

--- client/src/pages/login-page.jsx
+++ client/src/pages/login-page.jsx
@@ -1,6 +1,21 @@
+import { useUser } from '../hooks/useUser';
+import { useEffect } from "react"
+import { useNavigate } from 'react-router-dom';
+
 import { LoginForm } from "@/components/login-form"
 
 const LoginPage = () => {
+
+  const navigate = useNavigate();
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user !== null){
+      console.log('nav')
+      navigate('/my-tanks')
+    }
+  },[user])
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
