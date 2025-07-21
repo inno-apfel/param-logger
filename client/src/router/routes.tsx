2865e03561e0343a889607b123b34ae829d16bc2
Maximilian Wei <81603715+inno-apfel@users.noreply.github.com>
2025-07-20 18:48:30 -0700

    Fix type errors for react router

---
+++ client/src/router/routes.tsx
@@ -0,0 +1,34 @@
+import { type RouteObject } from "react-router-dom";
+
+import TankPage from "../pages/tank-page";
+import LoginPage from "../pages/login-page";
+import SignupPage from "../pages/signup-page";
+import Error404 from "../pages/error-404";
+import MyTanks from "../pages/my-tanks";
+import LandingPage from "../pages/landing-page";
+
+const routes: RouteObject[] = [
+  {
+    path: "my-tanks",
+    element: <MyTanks />,
+  },
+  {
+    path: "/dashboard/:tankId",
+    element: <TankPage />,
+    errorElement: <Error404 />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+  {
+    path: "signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+];
+
+export default routes;
