5510d2ffdce16c738e92e1c588712e79f6ca9035
Maximilian Wei <81603715+inno-apfel@users.noreply.github.com>
2025-07-27 21:59:22 -0700

    Update login/signup pages from temporary design

--- client/src/pages/login-page.tsx
+++ client/src/pages/login-page.tsx
@@ -17,9 +17,20 @@ const LoginPage = () => {
   },[user])
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="https://ui.shadcn.com/placeholder.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
       </div>
     </div>
   );
--- client/src/pages/signup-page.tsx
+++ client/src/pages/signup-page.tsx
@@ -2,9 +2,20 @@ import { SignupForm } from "@/components/signup-form"
 
 const SignupPage = () => {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <SignupForm />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="https://ui.shadcn.com/placeholder.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SignupForm />
+          </div>
+        </div>
       </div>
     </div>
   );
