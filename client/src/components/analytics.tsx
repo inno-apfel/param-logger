626f27c7201ea9021fbe8d05b8e5751c6dea5098
Maximilian Wei <81603715+inno-apfel@users.noreply.github.com>
2025-08-18 20:32:24 -0700

    fix: height of create parameter skeleton

--- client/src/components/analytics.tsx
+++ client/src/components/analytics.tsx
@@ -67,21 +67,22 @@ function Analytics() {
         refreshData={fetchObservations}
       >
         <Card className="relative m-2 grow-1 aspect-[5/2] bg-transparent border-2 border-dashed border-gray-300 rounded-lg p-6  hover:border-blue-400 hover:bg-blue-50 cursor-pointer transition-colors">
-            
             <CardHeader>
+                <div className="flex justify-between relative">
                   <CardTitle className='text-transparent'>
                     Hello
                   </CardTitle>
-              <CardDescription></CardDescription>
+                </div>
+              <CardDescription className='text-transparent'>
+                World
+              </CardDescription>
             </CardHeader>
-          <CardContent className='text-transparent h-full'>
-            World
+          <CardContent className='h-full'>
             <div className="flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
               <Plus className="text-gray-400 size-10"/>
               <span className="text-gray-600 font-medium">Add New Parameter</span>
               <span className="text-gray-400 text-sm">Click to create</span>
             </div>
-            
           </CardContent>
         </Card>
       </CreateEntityDialog>
