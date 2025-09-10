08e93f63871ce7ed9500d626c2552689b50fde5d
Maximilian Wei <81603715+inno-apfel@users.noreply.github.com>
2025-09-09 22:42:04 -0700

    fix: switch frontend enviroment variables to use VITE_ prefix

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,14 +1,7 @@
 import axios from 'axios'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const isProd = process.env.NODE_ENV === 'production';
 
 const api = axios.create({
-    baseURL: isProd 
-        ? 'https://api.nousdb.com'
-        : 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_SERVER_URL || 'http://localhost:8080',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
