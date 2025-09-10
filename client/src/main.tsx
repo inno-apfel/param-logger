35d33002d76c8541653761d4c869cb2000d9938f
Maximilian Wei <81603715+inno-apfel@users.noreply.github.com>
2025-09-09 17:33:06 -0700

    fix: Switch to hashRouter for gh-pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import { UserProvider } from '@/context/user/provider';
 import routes from "@/router/routes";
 import '@/index.css';
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
